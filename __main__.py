03b6cb6c0bc5155186c76f51b4557a24445023ab
Michael Swan <michael@michaelswan.io>
2013-04-27 22:35:13 -0400

    Finished the file parser... maybe.

--- __main__.py
+++ __main__.py
@@ -53,24 +53,65 @@ def parse_level_file(filename) :
 
     levels = []
     level_number = 0
-    level_text_lines = []
+    map_text_lines = []
     map = []
 
-    for i in range(len(contents)) :
+    for line_number in range(len(contents)) :
         # Current line, without linebreaks
-        line = contents[i].rstrip('\r\n')
+        line = contents[line_number].rstrip('\r\n')
 
         # Ignore everything after comments
         if ';' in line :
             line = line[:line.find(';')]
 
         if line != '' : # Current line is a part of the map
-            level_text_lines.append(line)
-        elif line == '' and len(level_text_lines) > 0 :
-            # Find the longest row in the map
+            map_text_lines.append(line)
+        elif line == '' and len(map_text_lines) > 0 :
+            # Find the longest row in the map, set width to that value
+            width = -1
+            for line in map_text_lines :
+                if width < len(line) :
+                    width = len(line)
+
+            # Set the height of the map to the map_text_lines length
+            height = len(map_text_lines)
+
             # Add spaces to the end of shorter lines to ensure that the content is rectangular.
-            # Convert level_text_lines to a level object
-            pass
+            for i in range(len(map_text_lines)) :
+                map_text_lines[i] += ' ' * (width - len(map_text_lines[i]))
+
+            # Convert map_text_lines to a level objects 
+            for x in range(width) :
+                map.append([])
+            for y in range(len(map_text_lines)):
+                for x in range(width) :
+                    map[x].append(map_text_lines[y][x])
+
+            # Find the starting location
+            start_x = None
+            start_y = None
+            goal_x = None
+            goal_y = None
+            for x in range(width) :
+                for y in range(height) :
+                    current_square = map[x][y]
+                    if current_square == '@' :
+                        start_x = x
+                        start_y = y
+                    elif current_square == '.' :
+                        goal_x = x
+                        goal_y = y
+            level = {'width': width,
+                     'height': height,
+                     'map': map,
+                     'start': { 'x': start_x, 'y': start_y },
+                     'goal':  { 'x': goal_x,  'y': goal_y  }
+                    }
+            levels.append(level)
+
+            map_text_lines = []
+            map = []
+    return levels
 
 if __name__ == '__main__':
     start()
